8e6653bae0389b0b667e6f89fd747004e99b84a5
Phonggampanart Kaewta <basphong2542@gmail.com>
2020-03-06 14:07:11 +0700

    final2

--- src/app/customer/customer.page.ts
+++ src/app/customer/customer.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
 
 @Component({
   selector: 'app-customer',
@@ -8,46 +7,9 @@ import axios from 'axios';
 })
 export class CustomerPage implements OnInit {
 
-  name: string;
-  lastname: string;
-  detail: string;
-
-  customers: any;
-
-
   constructor() { }
 
-  ngOnInit()  {
-    this.loaddata();
+  ngOnInit() {
   }
 
-  delete_data(id: any) {
-    console.log(id);
-    axios.get('http://localhost/ionicserver/delete_profile.php?id=' + id)
-    .then(() => {
-      console.log("ลบข้อมูลเรียบร้อย");
-      this.loaddata();
-    });
-  }
-
-  loaddata() {
-    axios.get('http://localhost/ionicserver/get_customer.php').then((response) => {
-      //handle success
-        console.log(response.data);
-
-          this.customers = response.data;
-        // this.name = response.data[1].name;
-        // this.lastname = response.data[1].lastname;
-        // this.detail = response.data[1].detail;
-        
-      })
-      .catch((error) => {
-        // handle error
-          console.log(error);
-      })
-      .then(() => {
-        //always executed
-      });
-  }
 }
-
